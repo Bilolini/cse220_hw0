fd3b32696b6a38ed0ef6ed0568575c1ae9da342f
Bilol Meliboev <96336891+Bilolini@users.noreply.github.com>
2024-09-03 16:55:01 +0000

    trying out

--- src/main.c
+++ src/main.c
@@ -13,68 +13,67 @@ int main(){
             };
     
     // Printing the state of the game
-    int open;
-    open = 0;
-    for (int i = 0; i < 5; i++)
-    {
-        for (int j = 0; j < 5; j++)
-        {
-            if(board[i][j] == ' ') open++;
-            printf("%c", board[i][j]);
-            printf(" ");
-        }
-        printf("\n");
-    }
-    printf("This is open\n");
-    printf("%d\n", open);
-    while(open != 0){
-        // Getting the values from the user
-        char val;
-        int row, col;
-        bool ok = true;
-        printf("Choose a piece (x or o) or q to quit: ");
-        while(ok){
-            scanf("%c", &val);
-            if(val == 'q'){
-                break;
-            }
-            else if(val == 'x' || val == 'o'){
-                ok = false;
-                break;
-            }else{
-                printf("Invalid choice. Choose a piece (x or o) or q to quit: ");
-            }
-        }
-        
+    bool done;
+    done = false;
 
-        printf("Choose a row (0-4): ");
-        scanf("%d", &row);
-        while(!(row >=0 && row < 5)){
-            printf("Invalid choice. Choose a row (0-4): ");
-        };
-
-        printf("Choose a column (0-4): ");
-        scanf("%d", &col);
-        while(!(col >=0 && col < 5)){
-            printf("Invalid choice. Choose a column (0-4): ");
-        };
-
-        if(board[row][col] != " "){
-            printf("Invalid choice. That space is already occupied.\n");
-
-            for (int i = 0; i < 5; i++)
-            {
-                for (int j = 0; j < 5; j++)
+    while(!done){
+        for (int i = 0; i < 5; i++){
+            for (int j = 0; j < 5; j++)
                 {
                     printf("%c", board[i][j]);
                     printf(" ");
                 }
-                printf("\n");
+            printf("\n");
+        }
+        // Getting the values from the user
+        char val;
+        int row, col;
+        
+        printf("Choose a piece (x or o) or q to quit: ");
+        scanf("%c", &val);
+        printf("%c\n", val);
+        
+        int c;
+        while((c= getchar()) != '\n' && c != EOF); 
+        if(val == 'q'){
+            done = true;
+            break;
+        }
+        if(val != 'x' && val != 'o'){
+            while(!(val == 'x' || val == 'o')){
+                printf("Invalid choice.Choose a piece (x or o) or q to quit: ");
+                scanf("%c", &val);
+
+                if(val == 'q'){
+                    done = true;
+                    break;
+                }
             }
+        }
+        
+
+        // ROW
+        printf("Choose a row (0-4): ");
+        scanf("%d", &row);
+        while(!(row >=0 && row < 5)){
+            printf("Invalid choice. Choose a row (0-4): ");
+            scanf("%d", &row);
+        };
+
+        // COLUMN
+        printf("Choose a column (0-4): ");
+        scanf("%d", &col);
+        while(!(col >=0 && col < 5)){
+            printf("Invalid choice. Choose a column (0-4): ");
+            scanf("%d", &row);
+        };
+
+
+        if(&board[row][col] == "x" || &board[row][col] == "0"){
+            printf("Invalid choice. That space is already occupied.\n");
         }else{
             board[row][col] = val;
-            open--;
-            if(open == 0){
+            if(done == true){
                 printf("Congratulations, you have filled the board!\n");
                 for (int i = 0; i < 5; i++)
                     {
